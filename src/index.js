2e797282ec635f44c0c261cc294176e88614b117
Nick Johnson <nlj222@gmail.com>
2023-01-12 10:07:13 -0800

    main deliverables complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const text = document.querySelector("input[type=text]").value;
+      const newListItem = document.createElement('li');
+      newListItem.textContent = `${text} `;
+      document.querySelector('#tasks').appendChild(newListItem);
+      const button = newListItem.appendChild(document.createElement('button'));
+      button.textContent = ' X ';
+      button.addEventListener('click', (e) => {
+        e.target.parentNode.remove();
+      })
+      form.reset();
+    })
 });
